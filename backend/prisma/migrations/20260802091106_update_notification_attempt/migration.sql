61c159b5c4d763ed08e553816d980233aff797f3
ramnivas11 <sutharramnivas10@gmail.com>
2026-08-02 14:44:12 +0530

    feat: initialize database schema with Notification, NotificationAttempt, and Idempotency models and update migration to remove deprecated createdAt column

---
+++ backend/prisma/migrations/20260802091106_update_notification_attempt/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `NotificationAttempt` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "NotificationAttempt" DROP COLUMN "createdAt";
